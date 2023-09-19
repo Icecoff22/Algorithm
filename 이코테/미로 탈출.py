287aea9eac50593c31dbcce5313282f6a516e46c
icecoff22 <wjdtmd987@gmail.com>
2023-09-19 18:21:39 +0900

    bfs : 미로탈출 혼자풀기

--- 이코테/미로 탈출.py
+++ 이코테/미로 탈출.py
@@ -8,14 +8,14 @@ def bfs(x, y):
     while queue:
         x, y = queue.popleft()
         for i in range(4):
-            ax = x+dx[i]
-            ay = y+dy[i]
-            if ax<0 or ay<0 or ax>=N or ay>=M:
+            ax = x + dx[i]
+            ay = y + dy[i]
+            if ax<0 or ax>=N or ay<0 or ay>=M:
                 continue
             if arr[ax][ay]==0:
                 continue
             if arr[ax][ay]==1:
-                arr[ax][ay]= arr[x][y]+1
+                arr[ax][ay] = arr[x][y] + 1
                 queue.append((ax, ay))
     return arr[N-1][M-1]
 
@@ -26,4 +26,3 @@ dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
 print(bfs(0, 0))
-
