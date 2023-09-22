e9c6885858aae3574cacc66a50a83fc27c781fe7
icecoff22 <wjdtmd987@gmail.com>
2023-09-22 17:30:38 +0900

    sorted 내림차순 방법 추가

--- 이코테/정렬/계수 정렬.py
+++ 이코테/정렬/계수 정렬.py
@@ -12,6 +12,12 @@ for i in range(len(count)):
 res = sorted(array)
 print(res)
 
+
+# key 값대로 정렬
+# sorted(정렬할 리스트, 정렬방식(함수로))
+# 아래 예시에서 setting함수에 의해 어느 key값으로 정렬할지 선택
+# 아래 예시에서 한글 순서대로 정렬하려면 data[0]을, 숫자 순서대로 정렬하려면 data[1]을 리턴해야함.
+# 내림차순으로 하려면 sorted(정렬할 리스트, key=정렬대상, reverse=True/False)
 array2 = [('바나나', 2), ('사과', 5), ('당근', 3)]
 
 def setting(data):
