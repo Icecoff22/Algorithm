b7dd86fcbab13aee3994f5f1829d6bc250453b25
icecoff22 <wjdtmd987@gmail.com>
2023-09-25 20:43:50 +0900

    [modify] 이진탐색 : 재귀.py

--- 이코테/이진탐색/재귀.py
+++ 이코테/이진탐색/재귀.py
@@ -1,24 +1,21 @@
 def binary_search(start, end, arr, target):
     if start>=end:
         return None
+    
     mid = (start+end)//2
     
     if arr[mid]==target:
         return mid
+    elif arr[mid]>target:
+        return binary_search(mid+1, end, arr, target)
+    else:
+        return binary_search(start, mid-1, arr, target)
     
-    if arr[mid]>target:
-        binary_search(mid+1,n-1,arr,target)
-    if arr[mid]<target:
-        binary_search(0, mid-1,arr,target)
-    
-    
-    
-
 n, target = map(int, input().split())
 arr = list(map(int, input().split()))
 
 res = binary_search(0, n-1, arr, target)
 if res==None:
-    print('none')
+    print('None')
 else:
     print(res+1)
