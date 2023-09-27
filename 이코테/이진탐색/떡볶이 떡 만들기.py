f39f223c92bf2d636ba20a58fdb1f4693f87f854
icecoff22 <wjdtmd987@gmail.com>
2023-09-27 16:25:39 +0900

    이진탐색 : 떡볶이 떡 만들기.py + 해설

--- 이코테/이진탐색/떡볶이 떡 만들기.py
+++ 이코테/이진탐색/떡볶이 떡 만들기.py
@@ -2,18 +2,20 @@ N, M = map(int, input().split())
 arr = list(map(int, input().split()))
 
 start = 0
-end = max(arr)
+end = max(arr) #절단기의 최저,최고점 초기화
 
-target = 0
-while (start<end):
-    res = 0
+while start<end:
+    length = 0
     mid = (start+end)//2
     for i in arr:
-        if i>mid:
-            res += i - mid
-    if res<M:
+        if mid<i:
+            length += i - mid
+    if length<M:
         end = mid - 1
+    elif length>M:
+        res = mid
+        start = mid +1
     else:
-        target = mid
-        start = mid+1
-print(target)
+        res = mid
+        start = mid +1
+print(res)
