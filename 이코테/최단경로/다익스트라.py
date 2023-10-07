664537cee82f7bc50becbe434f144bf0fa5b06a6
icecoff22 <wjdtmd987@gmail.com>
2023-10-07 09:42:56 +0900

    [modify] 다익스트라, 플로이드워셜

--- 이코테/최단경로/다익스트라.py
+++ 이코테/최단경로/다익스트라.py
@@ -1,20 +1,21 @@
-import heapq
 import sys
+import heapq
+from collections import deque
 input = sys.stdin.readline
 INF = int(1e9)
-
 n, m = map(int, input().split())
 start = int(input())
+
 graph = [[] for i in range(n+1)]
-distance = [INF] * (n+1)
 
 for i in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b,c))
-    
+
+distance = [INF] * (n+1)
 def dijkstra(start):
     q = []
-    heapq.heappush(q,(0,start))
+    heapq.heappush(q, (0,start))
     distance[start] = 0
     
     while q:
@@ -27,12 +28,8 @@ def dijkstra(start):
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
 
+
+
+
 dijkstra(start)
- 
-#모든 노드로 가기 위한 최단 거리 출력
-for i in range(1, n+1):
-    #도달할 수 없는 경우, 무한 출력
-    if distance[i]==INF:
-        print("INF")
-    else:
-        print(distance[i])
+print(distance, end=' ')
