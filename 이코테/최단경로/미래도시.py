50afd856a6d54ac672ef437409ab8bc855f8403c
icecoff22 <wjdtmd987@gmail.com>
2023-10-11 22:23:45 +0900

    미래도시.py

--- 이코테/최단경로/미래도시.py
+++ 이코테/최단경로/미래도시.py
@@ -3,21 +3,28 @@ import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 n, m = map(int, input().split())
-graph = [[] for i in range(n+1)]
-distance = [INF] * (n+1)
+graph = [[INF]*(n+1) for i in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i==j:
+            graph[i][j]=0
+
 for i in range(m):
     a,b = map(int, input().split())
-    graph[a].append(b)
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, start)
-    distance[start] = 0
+    graph[a][b] = 1
+    graph[b][a] = 1
     
-    while q:
-        now = heapq.heappop(q)
-        for i in graph[now]:
-            if 1 < 
+x, k = map(int, input().split())
 
 
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
 
-dijkstra(1)
+distance = graph[1][k] + graph[k][x]
+
+if distance >= INF:
+    print("-1")
+else:
+    print(distance)
