35bbd247616979358c1541e534a4f89856da9561
icecoff22 <wjdtmd987@gmail.com>
2023-11-18 17:24:32 +0900

    그리디 : 무지의 먹방 라이브

--- 이코테/그리디/무지의 먹방 라이브.py
+++ 이코테/그리디/무지의 먹방 라이브.py
@@ -1,28 +1,23 @@
 import heapq
 
 def solution(food_times, k):
-    if sum(food_times)<=k:
-        return -1
-    
+
     q = []
     for i in range(len(food_times)):
         heapq.heappush(q,(food_times[i],i+1))
+    previous = 0
+    food_num = len(food_times)
     
-    sum_time = 0
-    previous_time = 0
-    length = len(food_times)
-    
-    while(sum_time + (q[0][0]-previous_time)*length <=k):
-        now_time = heapq.heappop(q)[0]
-        sum_time += (now_time-previous_time)*length
-        length-=1
-        previous_time = now_time
-    
-    res = sorted(q,key = lambda x:x[1])
-    
-    return res[(k-sum_time)%length][1]
-
-food_times = [8,6,4]
-k=15
-
+    while q:
+        t = (q[0][0] - previous) * food_num
+        if k>=t:
+            k-=t
+            previous, _ = heapq.heappop(q)
+            food_num-=1
+        else:
+            idx = k%food_num
+            q.sort(key=lambda x:x[1])
+            return q[idx][1]
+food_times=[3,1,2]
+k=5
 print(solution(food_times,k))
