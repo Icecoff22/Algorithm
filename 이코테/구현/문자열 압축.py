2fe9ee58624dae63bd3663a881ca0795ed2462d3
icecoff22 <wjdtmd987@gmail.com>
2023-11-23 22:00:00 +0900

    구현 : 문자열 압축

--- 이코테/구현/문자열 압축.py
+++ 이코테/구현/문자열 압축.py
@@ -1,26 +1,23 @@
 def solution(s):
-
-    min_len = len(s)
-
-    for unit in range(1, (len(s)//2)+1):
-        result = ""    
-        previous = s[0:unit]
-        count = 1
-        for idx in range(unit, len(s), unit):
-            now = s[idx:idx+unit]
-            if (now == previous):
-                count += 1
+    result = []
+    
+    for i in range(1, (len(s)//2)+1):
+        b=''
+        tmp = s[:i]
+        cnt = 1
+        
+        for j in range(i,len(s),i):
+            if tmp==s[j:j+i]:
+                cnt+=1
             else:
-                if (count >= 2):
-                    result += str(count) + previous
+                if cnt==1:
+                    b = b + tmp
                 else:
-                    result += previous
-                previous = now
-                count = 1
-        if (count >= 2):
-            result += str(count) + previous
+                    b = b + str(cnt) + tmp
+        if cnt==1:
+            b = b + tmp
         else:
-            result += previous    
-        min_len = min(min_len, len(result))
+            b = b + str(cnt) + tmp
+        result.append(b)
 
-    return min_len
+    return min(result)
