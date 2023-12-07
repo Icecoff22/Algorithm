e15fe86a733f8710a6e3ace2632bab624e069dfa
icecoff22 <wjdtmd987@gmail.com>
2023-12-07 18:46:54 +0900

    adventofcode day2_part2

---
+++ adventofcode/day2_part2.py
@@ -0,0 +1,24 @@
+red_num,green_num,blue_num = 0,0,0
+color_dict = {'red': red_num, 'green': green_num, 'blue': blue_num}
+sum=0
+while True:
+    res=1  
+    a = input()
+    if a=='':
+        break
+    _, line = a.split(':')
+    for event in line.split(';'):
+        for balls in event.split(','):
+            n,color = balls.split()
+            if int(n) > color_dict.get(color,0):
+                color_dict[color] = int(n)
+                
+    keys = color_dict.keys()    
+    for i in keys:
+        res*=color_dict[i]
+        color_dict[i]=0
+    print(res)
+    sum+=res
+
+print(sum)
+    
