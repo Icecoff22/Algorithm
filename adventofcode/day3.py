1b626881ec4960aed954b85a2c797e54b91ca584
icecoff22 <wjdtmd987@gmail.com>
2023-12-12 13:56:56 +0900

    adventofcode day3 part1

---
+++ adventofcode/day3.py
@@ -0,0 +1,54 @@
+arr = []
+check = []
+start_idx = -1
+sum = 0
+while True:
+    a = input()
+    if a=='':
+        break
+    arr.append(a)
+check = [[0]*len(arr[0]) for i in range(len(arr))]
+for i in range(len(arr)):#기호 주변 반경에 드는 인덱스 0에서 1로 변환
+    for j in range(len(arr[i])):
+        if not (arr[i][j].isdigit() or arr[i][j]=='.'):
+            for k in range(i-1,i+2):
+                for l in range(j-1,j+2):
+                    if k<0 or l<0 or k>=len(arr) or l>=len(arr[0]):
+                        continue
+                    check[k][l] = 1
+for i in range(len(check)):
+    for j in range(len(check[i])):
+        print(check[i][j],end=' ')
+    print()
+for i in range(len(arr)):
+    arr22 = []
+    for j in range(len(arr[i])):
+        possible = False
+        if arr[i][j].isdigit() and start_idx==-1:
+            start_idx=j
+        elif not arr[i][j].isdigit() and start_idx!=-1:
+            last_idx=j
+            for k in range(start_idx,last_idx):
+                if check[i][k]==1:
+                    possible = True
+                    break
+            if possible:
+                arr22.append(int(arr[i][start_idx:last_idx]))
+                sum+=int(arr[i][start_idx:last_idx])
+            start_idx=-1
+        elif arr[i][j].isdigit() and j==len(arr[i])-1:
+            last_idx=j+1
+            for k in range(start_idx,last_idx):
+                if check[i][k]==1:
+                    possible = True
+                    break
+            if possible:
+                arr22.append(int(arr[i][start_idx:last_idx]))
+                sum+=int(arr[i][start_idx:last_idx])
+            start_idx=-1
+    for k in arr22:
+        print(k,end=' ')
+    print()
+print(sum)
+
+            
