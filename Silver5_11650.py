8dff51a98fd99d32a6ed75bcc04abc7e20299855
icecoff22 <wjdtmd987@gmail.com>
2024-01-22 00:28:47 +0900

    refactor : add another solving

--- Silver5_11650.py
+++ Silver5_11650.py
@@ -28,4 +28,16 @@ for i in arr:
 #   arr.append([a, b])
 # arr.sort()
 # for i in arr:
-#   print(i[0], i[1])      
+#   print(i[0], i[1])
+
+# 범위가 [-100,000, 100,000] 이므로 나눠서 오름차순 정렬.
+# def get_pos(nums):
+#     i, j = nums.split()
+#     return int(i) + int(j)/1000000
+
+# def get_answer():
+#     nums_list = sys.stdin.readlines()[1:]
+#     nums_list.sort(key=lambda nums: get_pos(nums))
+#     sys.stdout.write("".join(nums_list))
+
+# get_answer()
