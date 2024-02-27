80f7f9669029c841dcdcbcb69fe5862b59b08fee
icecoff22 <icecoff2222@gmail.com>
2024-02-27 23:47:36 +0900

    Bronze2_10813.py

---
+++ Bronze/Bronze2_10813.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+arr = [i+1 for i in range(n)]
+
+for i in range(m):
+    a, b = map(int, input().split())
+    arr[a-1], arr[b-1] = arr[b-1], arr[a-1]
+for i in range(n):
+    print(arr[i], end=' ')
