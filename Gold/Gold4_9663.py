1367842df5e0dfaef3b09ac2ca4644a8a58c360b
icecoff22 <icecoff2222@gmail.com>
2024-02-29 13:58:36 +0900

    Gold4_9663.py

--- Gold/Gold4_9663.py
+++ Gold/Gold4_9663.py
@@ -3,6 +3,7 @@ def is_promising(x):
     for i in range(x):
         if arr[i] == arr[x] or abs(x-i)==abs(arr[x]-arr[i]):
             return False
+        
     return True
 
 def n_queen(x):
@@ -10,18 +11,15 @@ def n_queen(x):
     if x==n:
         ans+=1
         return
+    
     for i in range(n):
-        if visited[i] == False:
-            arr[x] = i
-            if is_promising(x):
-                visited[i]==True
-                n_queen(x+1)
-                visited[i]==False
+        arr[x] = i
+        if is_promising(x):
+            n_queen(x+1)
 
 n = int(sys.stdin.readline())
 ans = 0
 arr = [0] * n
-visited = [False] * n
 
 
 n_queen(0)
