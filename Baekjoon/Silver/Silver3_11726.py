4fcf78ef8a6a62e4a42469835602cfaa3dea1b56
icecoff22 <icecoff2222@gmail.com>
2024-07-12 15:52:52 +0900

    240712

--- Baekjoon/Silver/Silver3_11726.py
+++ Baekjoon/Silver/Silver3_11726.py
@@ -1,10 +1,10 @@
 n = int(input())
 
-arr = [0] * (n+1)
+arr = [0] * (1001)
 arr[1] = 1
 arr[2] = 2
 
 for i in range(3, n+1):
-    arr[i] = arr[i-1] + arr[i-2]
+    arr[i] = (arr[i-1] + arr[i-2]) % 10007
 
-print(arr[n])
+print(arr[n] % 10007)
