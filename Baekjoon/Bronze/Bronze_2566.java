94f319f44635a0d5b728d19e04cbd8be32c5cdf8
icecoff22 <icecoff2222@gmail.com>
2025-01-02 23:56:40 +0900

    250102

--- Baekjoon/Bronze/Bronze_2566.java
+++ Baekjoon/Bronze/Bronze_2566.java
@@ -5,31 +5,23 @@ public class Main {
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int max = Integer.MIN_VALUE;
-        int maxI = 0;
-        int maxJ = 0;
+        int max = 0;  
+        int row = 1; 
+        int column = 1;
 
-        int arr[][] = new int[9][9];
-        for (int i = 0; i < 9; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 9; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (max < arr[i][j]) {
-                    maxI = i;
-                    maxJ = j;
-                    max = arr[i][j];
+        for (int i = 1; i <= 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j<= 9; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num > max) {
+                    max = num;
+                    row = i;
+                    column = j;
                 }
             }
         }
-
         System.out.println(max);
-        System.out.println(maxI + " " + maxJ);
+        System.out.println(row + " " + column);
     }
 }
