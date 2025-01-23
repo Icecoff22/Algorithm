0c8cb98f1196f4261315168ae62a4a3205646b16
icecoff22 <icecoff2222@gmail.com>
2025-01-23 23:17:07 +0900

    250123

---
+++ Baekjoon/Bronze/Bronze_24265.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Bronze_24265 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+        br.close();
+
+        System.out.println((n*(n-1))/2);
+        System.out.println(2);
+    }
+}
