907b558bcc8278c8e5e3679f09eceab4f1ecea8d
icecoff22 <icecoff2222@gmail.com>
2025-02-14 11:45:18 +0900

    250214

---
+++ Baekjoon/Silver/Silver1_11660.java
@@ -0,0 +1,55 @@
+package Baekjoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Silver1_11660 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N + 1][N + 1];
+        int[][] sumArr = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == 1) {
+                    sumArr[i][j] = sumArr[i][j - 1] + arr[i][j];
+                    continue;
+                }
+                if (j == 1) {
+                    sumArr[i][j] = sumArr[i - 1][j] + arr[i][j];
+                    continue;
+                }
+                sumArr[i][j] = sumArr[i][j -1] + sumArr[i - 1][j] - sumArr[i -1][j - 1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            if (x1 == x2 && y1 == y2) {
+                System.out.println(arr[x1][y1]);
+            }
+            else {
+                System.out.println(sumArr[x2][y2] - sumArr[x1 - 1][y2] - sumArr[x2][y1 - 1] + sumArr[x1 - 1][y1 - 1]);
+            }
+        }
+
+    }
+}
