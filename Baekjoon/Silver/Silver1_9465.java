4be40ea7037cf171c504e5856f04888fd942013d
icecoff22 <icecoff2222@gmail.com>
2025-03-02 19:37:10 +0900

    fix : 250302

--- Baekjoon/Silver/Silver1_9465.java
+++ Baekjoon/Silver/Silver1_9465.java
@@ -24,6 +24,7 @@ public class Silver1_9465 {
 
             if (n == 1) {
                 System.out.println(Math.max(arr[0][0], arr[1][0]));
+                continue;
             }
 
             res[0][0] = arr[0][0];
